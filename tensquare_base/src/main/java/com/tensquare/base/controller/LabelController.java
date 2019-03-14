35ad8719b2527d6827cdc931af38e9bd81ada160
your name <your email>
2019-03-14 16:09:21 +0800

    Initial commit

---
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -0,0 +1,63 @@
+package com.tensquare.base.controller;
+
+import com.tensquare.base.Service.LabelService;
+import com.tensquare.base.pojo.Label;
+import entity.PageResult;
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/label")
+public class LabelController {
+
+    @Autowired
+    private LabelService labelService;
+
+    @GetMapping()
+    public Result findAll(){
+        return new Result(true, StatusCode.OK, "查询成功", labelService.findAll());
+    }
+
+    @GetMapping("/{labelId}")
+    public Result findById(@PathVariable("labelId") String labelId){
+        return new Result(true, StatusCode.OK, "查询成功", labelService.findById(labelId));
+    }
+
+    @PostMapping()
+    public Result save(@RequestBody Label label){
+        labelService.save(label);
+        return new Result(true, StatusCode.OK, "添加成功");
+    }
+
+    @PutMapping()
+    public Result update(@PathVariable String labelId, @RequestBody Label label){
+        label.setId(labelId);
+        labelService.update(label);
+        return new Result(true, StatusCode.OK, "更新成功");
+    }
+
+    @DeleteMapping()
+    public Result delete(@PathVariable String labelId){
+        labelService.deleteById(labelId);
+        return new Result(true, StatusCode.OK, "删除成功");
+    }
+
+    @PostMapping("/search")
+    public Result findSearch(@RequestBody Label label){
+        List<Label> list = labelService.findSearch(label);
+        System.out.println(list);
+        return new Result(true, StatusCode.OK, "查询成功", list);
+    }
+
+    @PostMapping("/search/{page}/{size}")
+    public Result pageQuery(@PathVariable("page") int page, @PathVariable("size") int size, @RequestBody Label label){
+        Page<Label> pageDate = labelService.pageQuery(label, size, page);
+        return new Result(true, StatusCode.OK, "查询成功", new PageResult<Label>(pageDate.getTotalElements(), pageDate.getContent()));
+    }
+}
